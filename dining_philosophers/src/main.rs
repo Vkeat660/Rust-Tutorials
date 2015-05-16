01d4b5827c6b474f55e05caea6ce63d591e8e9c1
Vivian Keating <Vkeat660@gmail.com>
2015-05-16 19:08:00 -0400

    Added delay to eating

--- dining_philosophers/src/main.rs
+++ dining_philosophers/src/main.rs
@@ -1,3 +1,5 @@
+use std::thread;
+
 struct Philosopher {
     name: String,
 }
@@ -10,7 +12,13 @@ impl Philosopher {
     }
 
     fn eat(&self) {
+
+        println!("{} is eating.", self.name);
+
+        thread::sleep_ms(1000);
+
         println!("{} is done eating.", self.name);
+
     }
 }
 
