7cf1023d3304faebe3179d45e45960f78ae6f33b
Vivian Keating <Vkeat660@gmail.com>
2015-05-16 17:47:10 -0400

    Guessing game now includes random number

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,24 @@
+extern crate rand;
+
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+	
+    println!("Guess the number!");
+    
+    let secret_number = rand::thread_rng().gen_range(1,101);
+
+    println!("The secret number is: {}", secret_number);
+
+    println!("Please input your guess.");
+
+    let mut guess = String::new();
+
+    io::stdin().read_line(&mut guess)
+               .ok()
+               .expect("Failed to read Line");
+
+    println!("You guessed: {}", guess);
+
 }
